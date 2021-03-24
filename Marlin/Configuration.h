e5203e6ba9d219f59c6a5c192747fa44b0c34329
K0rwen <korwen@gmail.com>
2021-03-24 11:46:16 -0500

    Updating Z offset and tuning extruder calibration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 1600, 414.778 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 1600, 437.636 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -990,7 +990,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 48.38, 0, -1.28 }
+#define NOZZLE_TO_PROBE_OFFSET { 48.38, 0, -1.98 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
